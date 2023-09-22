de35c2e1af1caddd3c9f58e617db326b0984744e
Usmonaliyev <uzayyub2007@gmail.com>
2023-09-22 17:03:39 +0500

    awhdaudywv

--- lib/src/view/widget/main_textfield.dart
+++ lib/src/view/widget/main_textfield.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MainTextfield extends StatelessWidget {
+class MainTextfield extends StatefulWidget {
   const MainTextfield({super.key});
 
+  @override
+  State<MainTextfield> createState() => _MainTextfieldState();
+}
+
+class _MainTextfieldState extends State<MainTextfield> {
+  bool isTap = false;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,89 +24,98 @@ class MainTextfield extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
-            Image.asset(
-              "stop.png",
-              width: 16,
-              height: 16,
+            const SizedBox(width: 5),
+            InkWell(
+              onTap: () {
+                isTap = !isTap;
+                setState(() {});
+              },
+              child: Image.asset(
+                isTap ? "stop.png" : "plus.png",
+                width: 16,
+                height: 16,
+              ),
             )
           ],
         ),
         const SizedBox(
           height: 16,
         ),
-        Row(
-          children: [
-            Container(
-              width: 376,
-              height: 36,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(3),
-                  border: Border.all(
-                    color: const Color(0xFFB5BDC5),
-                    width: 1,
-                  )),
-              child: Row(
+        isTap
+            ? Row(
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: TextField(
-                        style: GoogleFonts.inter(
-                          color: const Color(0xFF344054),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
+                  Container(
+                    width: 376,
+                    height: 36,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        border: Border.all(
+                          color: const Color(0xFFB5BDC5),
+                          width: 1,
+                        )),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            child: TextField(
+                              style: GoogleFonts.inter(
+                                color: const Color(0xFF344054),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: 72,
+                          height: double.infinity,
+                          decoration: const BoxDecoration(
+                            color: Color(0xFF404E67),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(3),
+                              bottomLeft: Radius.circular(3),
+                            ),
+                          ),
+                          child: Text(
+                            "Excel",
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
+                  const SizedBox(width: 14),
                   Container(
                     alignment: Alignment.center,
-                    width: 72,
-                    height: double.infinity,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFF404E67),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(3),
-                        bottomLeft: Radius.circular(3),
-                      ),
+                    width: 100,
+                    height: 36,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CAF50),
+                      borderRadius: BorderRadius.circular(3),
                     ),
                     child: Text(
-                      "Excel",
+                      "Qo'shish",
                       style: GoogleFonts.inter(
+                        color: Colors.white,
                         fontWeight: FontWeight.w500,
                         fontSize: 16,
-                        color: Colors.white,
                       ),
                     ),
                   ),
                 ],
-              ),
-            ),
-            const SizedBox(width: 14),
-            Container(
-              alignment: Alignment.center,
-              width: 100,
-              height: 36,
-              decoration: BoxDecoration(
-                color: const Color(0xFF4CAF50),
-                borderRadius: BorderRadius.circular(3),
-              ),
-              child: Text(
-                "Qo'shish",
-                style: GoogleFonts.inter(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
+              )
+            : const SizedBox(),
       ],
     );
   }
