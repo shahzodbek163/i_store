e81bbfb1a6ca24e723cc556d5c076d7a4a7d84cd
Usmonaliyev <uzayyub2007@gmail.com>
2023-09-22 14:52:29 +0500

    yaqwtdyawdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:i_store/src/view/screen/main_data_screen.dart';
+import 'package:i_store/src/view/screen/test_screen.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MainDataScreen(),
+    home: TestScreen(),
   ));
 }
