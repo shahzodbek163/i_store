f7859e6721dd3d5d361e4ce1be08d422a20d1376
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-09-23 13:50:13 +0500

    qwe

---
+++ lib/src/view/widget/text_fields_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TextFieldsWidget extends StatelessWidget {
+  const TextFieldsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextField(
+          
+        ),
+      ),
+    );
+  }
+}
